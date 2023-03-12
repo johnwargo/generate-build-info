8d010d19373421ebf52e63598f8d1dcb25cc6a40
John M. Wargo <johnwargo@users.noreply.github.com>
2023-03-11 20:18:48 -0500

    Compiles, won't run

--- gen-build-info.js
+++ gen-build-info.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
 var HighlightType;
 (function (HighlightType) {
     HighlightType[HighlightType["Red"] = 0] = "Red";
@@ -11,44 +6,44 @@ var HighlightType;
     HighlightType[HighlightType["Green"] = 2] = "Green";
 })(HighlightType || (HighlightType = {}));
 // node modules
-var fs_1 = __importDefault(require("fs"));
-var path_1 = __importDefault(require("path"));
+import fs from 'fs';
+import path from 'path';
 // Third-party modules
-var boxen = require('boxen');
-var chalk = require('chalk');
-var yargs = require('yargs/yargs');
-var hideBin = require('yargs/helpers').hideBin;
+const boxen = require('boxen');
+const chalk = require('chalk');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
 // local constants
-var appName = 'Generate Build Info';
-var buildDate = new Date(Date.now());
-var inputFile = path_1.default.join(process.cwd(), 'package.json');
-var outputFileName = 'buildinfo.js';
-var red = HighlightType.Red;
-var yellow = HighlightType.Yellow;
-var green = HighlightType.Green;
+const appName = 'Generate Build Info';
+const buildDate = new Date(Date.now());
+const inputFile = path.join(process.cwd(), 'package.json');
+const outputFileName = 'buildinfo.js';
+const red = HighlightType.Red;
+const yellow = HighlightType.Yellow;
+const green = HighlightType.Green;
 var outputFolder;
 var outputFile;
 function writeConsole(color, highlightText, msg) {
     if (color == HighlightType.Red)
-        console.log(chalk.red("".concat(highlightText, ": ")) + msg);
+        console.log(chalk.red(`${highlightText}: `) + msg);
     if (color == HighlightType.Yellow)
-        console.log(chalk.yellow("".concat(highlightText, ": ")) + msg);
+        console.log(chalk.yellow(`${highlightText}: `) + msg);
     if (color == HighlightType.Green)
-        console.log(chalk.green("".concat(highlightText, ": ")) + msg);
+        console.log(chalk.green(`${highlightText}: `) + msg);
 }
 // Check our command-line argument(s)
-var argv = yargs(hideBin(process.argv)).argv;
+const argv = yargs(hideBin(process.argv)).argv;
 if (!argv.outputFolder) {
     writeConsole(red, 'Error', 'Output folder not specified\n');
     process.exit(1);
 }
-outputFolder = path_1.default.join(process.cwd(), argv.outputFolder);
-outputFile = path_1.default.join(outputFolder, outputFileName);
+outputFolder = path.join(process.cwd(), argv.outputFolder);
+outputFile = path.join(outputFolder, outputFileName);
 console.log(boxen(appName, { padding: 1 }));
 writeConsole(yellow, 'Output folder', outputFolder);
 writeConsole(yellow, 'Output file', outputFile);
 try {
-    if (!fs_1.default.existsSync(outputFolder)) {
+    if (!fs.existsSync(outputFolder)) {
         writeConsole(red, 'Error', 'Output folder does not exist\n');
         process.exit(1);
     }
@@ -59,7 +54,7 @@ catch (err) {
 }
 writeConsole(HighlightType.Yellow, '\nInput file', inputFile);
 try {
-    if (!fs_1.default.existsSync(inputFile)) {
+    if (!fs.existsSync(inputFile)) {
         writeConsole(red, 'Error', 'This is not a nodeJS project, cannot find `package.json` in this folder\n');
         process.exit(1);
     }
@@ -68,18 +63,18 @@ catch (err) {
     writeConsole(red, 'Error', err.message);
     process.exit(1);
 }
-var rawData = fs_1.default.readFileSync(inputFile);
-var packageDotJSON = JSON.parse(rawData.toString());
-var buildVersion = packageDotJSON.version;
+let rawData = fs.readFileSync(inputFile);
+let packageDotJSON = JSON.parse(rawData.toString());
+let buildVersion = packageDotJSON.version;
 writeConsole(green, 'Build version', buildVersion);
-writeConsole(green, 'Build date', "".concat(buildDate.toString(), " (").concat(buildDate.getTime().toString(), ")"));
-var outputStr = 'export const buildInfo = {\n';
-outputStr += "  buildVersion: \"".concat(buildVersion, "\",\n");
-outputStr += "  buildDate: ".concat(buildDate.getTime(), ",\n");
+writeConsole(green, 'Build date', `${buildDate.toString()} (${buildDate.getTime().toString()})`);
+let outputStr = 'export const buildInfo = {\n';
+outputStr += `  buildVersion: "${buildVersion}",\n`;
+outputStr += `  buildDate: ${buildDate.getTime()},\n`;
 outputStr += '}';
 console.log('\nWriting output file');
 try {
-    fs_1.default.writeFileSync(outputFile, outputStr, 'utf8');
+    fs.writeFileSync(outputFile, outputStr, 'utf8');
     writeConsole(green, 'Success', 'Output file written successfully\n');
 }
 catch (err) {
